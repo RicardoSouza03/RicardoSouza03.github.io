244089c86cf8d4c5e042875362ca98f93a6dbc52
Ricardo Souza <rl03souza@gmail.com>
2023-06-02 16:48:16 -0300

    Feat: create header component

---
+++ src/Components/Header.tsx
@@ -0,0 +1,79 @@
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Sobre', href: '#About', current: false },
+  { name: 'Projetos', href: '#Projects', current: false },
+  { name: 'Skills', href: '#Skils', current: false },
+  { name: 'Contato', href: '#Contact', current: false },
+]
+
+function classNames(...classes: Array<string>) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Header() {
+    return (
+        <Disclosure as="nav" className="bg-gray-800">
+        {({ open }: {open: boolean}) => (
+            <>
+            <div className="mx-auto max-w-7xl px-2">
+                <div className="relative flex h-16 items-center justify-between">
+                <div className="absolute inset-y-0 left-0 flex items-center 2xl:hidden xl:hidden md:hidden lg:hidden">
+                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Abrir menu</span>
+                    {open ? (
+                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                    </Disclosure.Button>
+                </div>
+                <div className="flex flex-1 ml-6 items-center phone:justify-center sm:justify-center justify-start">
+                    <div className="flex flex-shrink-0 items-center">
+                    <h1 className="text-white text-lg ml-6 block h-8 w-auto">Ricardo Souza</h1>
+                    </div>
+                    <div className="hidden xl:ml-6 2xl:block 2xl:ml-6 xl:block md:block md:ml-6 lg:block lg:ml-6">
+                    <div className="flex space-x-4">
+                        {navigation.map((item) => (
+                        <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            'rounded-md px-3 py-2 text-sm font-medium'
+                            )}
+                            aria-current={item.current ? 'page' : undefined}
+                        >
+                            {item.name}
+                        </a>
+                        ))}
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <Disclosure.Panel className="md:hidden">
+                <div className="space-y-1 px-2 pb-3 pt-2">
+                {navigation.map((item) => (
+                    <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'block rounded-md px-3 py-2 text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                    >
+                    {item.name}
+                    </Disclosure.Button>
+                ))}
+                </div>
+            </Disclosure.Panel>
+            </>
+        )}
+        </Disclosure>
+    )
+}
