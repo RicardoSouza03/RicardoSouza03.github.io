05b49f1926526768643f8c811a3042e9a2cd23a8
Ricardo Souza <rl03souza@gmail.com>
2023-06-05 17:17:19 -0300

    Style: change some styles in tecnologies

--- src/Components/Tecnologies.tsx
+++ src/Components/Tecnologies.tsx
@@ -2,14 +2,14 @@ import tecList from '../utils/SkillsList';
 
 export default function Tecnologies() {
     return (
-      <div className="bg-gray-800 py-8 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-center text-4xl font-semibold leading-8 text-white">
+      <div className="bg-white py-8 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6">
+          <h2 className="text-center text-4xl font-semibold leading-8 text-gray-800">
             Tecnologies learned
           </h2>
-          <div className="mx-auto mt-10 grid max-w-lg grid-cols-6 items-center gap-x-8 gap-y-10 phone:grid-cols-3 sm:max-w-xl sm:gap-x-10">
+          <div className="mx-auto mt-10 grid max-w-3xl border-4 px-6 py-4 grid-cols-6 items-center gap-x-8 gap-y-10 phone:grid-cols-3 sm:max-w-xl sm:gap-x-10">
             {tecList.map((skill: {name: string, src: string}) => (
-                <div key={skill.name} className="flex flex-col items-center">
+                <div key={skill.name} className="flex hover:scale-150 transition ease-in delay-150 flex-col items-center">
                     <img
                     className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
                     src={skill.src}
@@ -17,7 +17,7 @@ export default function Tecnologies() {
                     width={158}
                     height={48}
                     />
-                    <span className="font-mono text-white">{skill.name}</span>
+                    <span className="font-mono text-gray-800">{skill.name}</span>
                 </div>
             ))}
           </div>
